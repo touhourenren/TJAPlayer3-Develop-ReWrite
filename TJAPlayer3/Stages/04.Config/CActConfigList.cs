868b02b9cbeb85a09fc3fef9535c71a419681994
elizanan44 <kardor225864@yahoo.co.jp>
2020-12-19 21:57:07 +0900

    コンフィグ画面を抜けたときに音量が初期値になってしまうのを修正。

--- TJAPlayer3/Stages/04.Config/CActConfigList.cs
+++ TJAPlayer3/Stages/04.Config/CActConfigList.cs
@@ -1538,7 +1538,7 @@ namespace TJAPlayer3
 			this.ct三角矢印アニメ = new CCounter();
 
 			this.iSystemSoundType_initial			= this.iSystemSoundType.n現在選択されている項目番号;	// CONFIGに入ったときの値を保持しておく
-			// this.iSystemWASAPIBufferSizeMs_initial	= this.iSystemWASAPIBufferSizeMs.n現在の値;				// CONFIG脱出時にこの値から変更されているようなら
+			this.iSystemWASAPIBufferSizeMs_initial	= this.iSystemWASAPIBufferSizeMs.n現在の値;				// CONFIG脱出時にこの値から変更されているようなら
 			// this.iSystemASIOBufferSizeMs_initial	= this.iSystemASIOBufferSizeMs.n現在の値;				// サウンドデバイスを再構築する
 			this.iSystemASIODevice_initial			= this.iSystemASIODevice.n現在選択されている項目番号;	//
 			this.iSystemSoundTimerType_initial      = this.iSystemSoundTimerType.GetIndex();				//
@@ -1587,7 +1587,6 @@ namespace TJAPlayer3
 						soundDeviceType = ESoundDeviceType.Unknown;
 						break;
 				}
-
 				TJAPlayer3.Sound管理.t初期化( soundDeviceType,
 										this.iSystemWASAPIBufferSizeMs.n現在の値,
 										0,
