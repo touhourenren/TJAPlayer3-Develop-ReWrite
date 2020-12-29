0eb699c654b7da79f74e4f44192d95b7e08bb8a5
elizanan44 <kardor225864@yahoo.co.jp>
2020-12-29 19:58:54 +0900

    -

--- TJAPlayer3/Stages/08.Result/CStage結果.cs
+++ TJAPlayer3/Stages/08.Result/CStage結果.cs
@@ -118,19 +118,22 @@ namespace TJAPlayer3
 						}
 					}
 					this.n総合ランク値 = CScoreIni.t総合ランク値を計算して返す(this.st演奏記録.Drums, this.st演奏記録.Guitar, this.st演奏記録.Bass);
-					this.nクリア = (this.st演奏記録.Drums.nMiss数 == 0 && this.st演奏記録.Drums.fゲージ == 100) ? this.st演奏記録.Drums.nGreat数 == 0 ? 3 : 2 : this.st演奏記録.Drums.fゲージ >= 80 ? 1 : 0;
+					if (TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Dan && TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Tower)
+					{
+						this.nクリア = (this.st演奏記録.Drums.nMiss数 == 0 && this.st演奏記録.Drums.fゲージ == 100) ? this.st演奏記録.Drums.nGreat数 == 0 ? 3 : 2 : this.st演奏記録.Drums.fゲージ >= 80 ? 1 : 0;
 
-					if (this.st演奏記録.Drums.nスコア < 500000)
-					{
-						this.nスコアランク = 0;
-					}
-					else
-					{
-						for (int i = 0; i < 7; i++)
+						if (this.st演奏記録.Drums.nスコア < 500000)
 						{
-							if (this.st演奏記録.Drums.nスコア >= TJAPlayer3.stage演奏ドラム画面.ScoreRank.ScoreRank[i])
+							this.nスコアランク = 0;
+						}
+						else
+						{
+							for (int i = 0; i < 7; i++)
 							{
-								this.nスコアランク = i + 1;
+								if (this.st演奏記録.Drums.nスコア >= TJAPlayer3.stage演奏ドラム画面.ScoreRank.ScoreRank[i])
+								{
+									this.nスコアランク = i + 1;
+								}
 							}
 						}
 					}
@@ -173,8 +176,11 @@ namespace TJAPlayer3
 						ini.stセクション[0] = this.st演奏記録[0];
 					}
 
-					ini.stセクション[0].nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nクリア;
-					ini.stセクション[0].nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nスコアランク;
+					if(TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Dan && TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Tower)
+					{
+						ini.stセクション[0].nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nクリア;
+						ini.stセクション[0].nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nスコアランク;
+					}
 
 					// ラストプレイ #23595 2011.1.9 ikanick
 					// オートじゃなければプレイ結果を書き込む
@@ -208,21 +214,6 @@ namespace TJAPlayer3
 					}
 					#endregion
 				}
-
-				#region [ 選曲画面の譜面情報の更新 ]
-				//---------------------
-				if (!TJAPlayer3.bコンパクトモード)
-				{
-					Cスコア cスコア = TJAPlayer3.stage選曲.r確定されたスコア;
-
-					if(cスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] < nクリア)
-						cスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nクリア;
-
-					if (cスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] < nスコアランク)
-						cスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nスコアランク;
-				}
-				//---------------------
-				#endregion
 
 				// Discord Presenseの更新
 				Discord.UpdatePresence(TJAPlayer3.DTX.TITLE + ".tja", Properties.Discord.Stage_Result + (TJAPlayer3.ConfigIni.b太鼓パートAutoPlay == true ? " (" + Properties.Discord.Info_IsAuto + ")" : ""), TJAPlayer3.StartupTime);
@@ -450,10 +441,10 @@ namespace TJAPlayer3
 								TJAPlayer3.Skin.sound決定音.t再生する();
 								actFI.tフェードアウト開始();
 
-								foreach(var song in TJAPlayer3.Songs管理.list曲ルート)
-                                {
-									if(song.strジャンル == "最近遊んだ曲" && song.eノード種別 == C曲リストノード.Eノード種別.BOX)
-                                    {
+								foreach (var song in TJAPlayer3.Songs管理.list曲ルート)
+								{
+									if (song.strジャンル == "最近遊んだ曲" && song.eノード種別 == C曲リストノード.Eノード種別.BOX)
+									{
 										song.list子リスト.Add(TJAPlayer3.stage選曲.r確定された曲.Clone());
 
 										foreach (var song2 in song.list子リスト)
@@ -465,20 +456,70 @@ namespace TJAPlayer3
 												song2.BackColor = ColorTranslator.FromHtml("#164748");
 										}
 
-										if(song.list子リスト.Count >= 6)
-                                        {
-											song.list子リスト.RemoveAt(0);
-                                        }
+										if (song.list子リスト.Count >= 6)
+										{
+											song.list子リスト.RemoveAt(1);
+										}
 									}
 								}
 
+								if (TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Dan && TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Tower)
+								{
+									if (nスコアランク != 0)
+									{
+										if (TJAPlayer3.stage選曲.r確定されたスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] == 0)
+										{
+											TJAPlayer3.stage選曲.act曲リスト.ScoreRankCount[nスコアランク - 1] += 1;
+										}
+										else if (TJAPlayer3.stage選曲.r確定されたスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] < nスコアランク)
+										{
+											TJAPlayer3.stage選曲.act曲リスト.ScoreRankCount[nスコアランク - 1] += 1;
+											TJAPlayer3.stage選曲.act曲リスト.ScoreRankCount[TJAPlayer3.stage選曲.r確定されたスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] - 1] -= 1;
+										}
+									}
+
+									if (nクリア != 0)
+									{
+										if (TJAPlayer3.stage選曲.r確定されたスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] == 0)
+										{
+											TJAPlayer3.stage選曲.act曲リスト.CrownCount[nクリア - 1] += 1;
+										}
+										else if (TJAPlayer3.stage選曲.r確定されたスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] < nクリア)
+										{
+											TJAPlayer3.stage選曲.act曲リスト.CrownCount[nクリア - 1] += 1;
+											TJAPlayer3.stage選曲.act曲リスト.CrownCount[TJAPlayer3.stage選曲.r確定されたスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] - 1] += 1;
+										}
+										
+									}
+								}
+
+								#region [ 選曲画面の譜面情報の更新 ]
+								//---------------------
+								if (!TJAPlayer3.bコンパクトモード)
+								{
+									if (TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Dan && TJAPlayer3.stage選曲.n確定された曲の難易度 != (int)Difficulty.Tower)
+									{
+										Cスコア cスコア = TJAPlayer3.stage選曲.r確定されたスコア;
+
+										if (cスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] < nクリア)
+											cスコア.譜面情報.nクリア[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nクリア;
+
+										if (cスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] < nスコアランク)
+											cスコア.譜面情報.nスコアランク[TJAPlayer3.stage選曲.n確定された曲の難易度] = this.nスコアランク;
+									}
+								}
+								//---------------------
+								#endregion
+
 								if (TJAPlayer3.stage選曲.r現在選択中の曲.r親ノード != null)
 									TJAPlayer3.stage選曲.act曲リスト.tBOXを出る();
-
+                                else
+                                {
+									base.eフェーズID = CStage.Eフェーズ.共通_フェードアウト;
+									this.eフェードアウト完了時の戻り値 = E戻り値.完了;
 									TJAPlayer3.Skin.bgmリザルト音.t停止する();
 									TJAPlayer3.Skin.sound決定音.t再生する();
-								base.eフェーズID = CStage.Eフェーズ.共通_フェードアウト;
-								this.eフェードアウト完了時の戻り値 = E戻り値.完了;
+								}
 							}
 						}
 					}
