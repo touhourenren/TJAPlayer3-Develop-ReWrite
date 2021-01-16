d9fb6d54ad48a747461b33c5e905725c6d696a25
Reiya0917milk <76889349+Reiya0917milk@users.noreply.github.com>
2021-01-16 13:17:19 +0900

    2Pの時に段位・特訓モードに入れないように

--- TJAPlayer3/Stages/05.SongSelect/CStage選曲.cs
+++ TJAPlayer3/Stages/05.SongSelect/CStage選曲.cs
@@ -544,8 +544,11 @@ namespace TJAPlayer3
                         #region [ F7 TokkunMode ]
                         if (TJAPlayer3.Input管理.Keyboard.bキーが押された((int)SlimDXKeys.Key.F7))
                         {
-                            TJAPlayer3.Skin.sound変更音.t再生する();
-                            C共通.bToggleBoolian(ref TJAPlayer3.ConfigIni.bTokkunMode);
+                            if (TJAPlayer3.ConfigIni.nPlayerCount != 2)
+                            {
+                                TJAPlayer3.Skin.sound変更音.t再生する();
+                                C共通.bToggleBoolian(ref TJAPlayer3.ConfigIni.bTokkunMode);
+                            }
                         }
                         #endregion
                         #region [ F8 ランダム選曲 ]
